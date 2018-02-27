8faed02f6286a97f68b40152e1e5581c460e6f81
Elyes DOUDECH <elyes.doudech@igtp3-mac09.isim.intra>
2018-02-27 16:52:49 +0100

    Initialize

---
+++ My_Pk_Et_Moi/My_Pk_Et_Moi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My_Pk_Et_Moi
+//
+//  Created by Elyes DOUDECH on 27/02/2018.
+//  Copyright © 2018 Elyes DOUDECH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
