5ee37e473de3fb238b7fc6d6125a4ce887c969f6
Elyes DOUDECH <elyes.doudech@igtp4-mac07.isim.intra>
2018-03-21 08:14:09 +0100

    nothing new

---
+++ My_Pk_Et_Moi/My_Pk_Et_Moi/TraitementNotificationViewController.swift
@@ -0,0 +1,37 @@
+//
+//  TraitementNotificationViewController.swift
+//  My_Pk_Et_Moi
+//
+//  Created by Elyes DOUDECH on 21/03/2018.
+//  Copyright © 2018 Elyes DOUDECH. All rights reserved.
+//
+
+import UIKit
+
+class TraitementNotificationViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
