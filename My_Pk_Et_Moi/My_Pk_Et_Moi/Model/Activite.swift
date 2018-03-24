621d18bb9c68654c625566f1e9ce99b267e515de
Amin BAZAZ <amin.bazaz@igtp4-mac07.isim.intra>
2018-03-24 11:37:27 +0100

    suppression objectif

---
+++ My_Pk_Et_Moi/My_Pk_Et_Moi/Model/Activite.swift
@@ -0,0 +1,20 @@
+//
+//  Activite.swift
+//  My_Pk_Et_Moi
+//
+//  Created by Amin BAZAZ on 24/03/2018.
+//  Copyright © 2018 Elyes DOUDECH. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Activite{
+    func getAllActivity() -> NSFetchedResultsController<Activite>{
+        let request : NSFetchRequest<Activite> = Activite.fetchRequest()
+        request.sortDescriptors = [NSSortDescriptor(key:#keyPath(Activite.nom),ascending:true)]
+        let fetchResultController = NSFetchedResultsController(fetchRequest: request, managedObjectContext: CoreDataManager.context, sectionNameKeyPath: nil, cacheName: nil)
+        fetchResultController.delegate = self as? NSFetchedResultsControllerDelegate
+        return fetchResultController
+    }
+}
---
+++ My_Pk_Et_Moi/My_Pk_Et_Moi/Model/Evaluation.swift
@@ -0,0 +1,14 @@
+//
+//  Evaluation.swift
+//  My_Pk_Et_Moi
+//
+//  Created by Amin BAZAZ on 24/03/2018.
+//  Copyright © 2018 Elyes DOUDECH. All rights reserved.
+//
+
+import Foundation
+
+extension Evaluation{
+    
+
+}
